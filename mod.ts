5ac85eea4913bc674f140e0d2451316f2f530be3
kevinelong <kevinelong@gmail.com>
2021-07-03 20:19:49 -0700

    add args and assert

--- mod.ts
+++ mod.ts
@@ -1,13 +1,25 @@
-const toKV = (attrs: { [k: string]: string }) => Object.keys(attrs).map((a: string) => ` ${a}="${attrs[a]}"`).join(",");
+// INTERFACES
+interface Args {
+    attrs?: {},
+    content?: any[]
+}
+
+// UTILS
+const toKV = (attrs: {} = {}) => Object.entries(attrs).map(kv => `${kv[0]}="${kv[1]}"`).join(" ");
 const group = (list: any) => list.join('');
-const get_content = (content: any) => typeof content === 'object' ? group(content) : content;
+const get_content = (args?: Args) => args === undefined || !args.hasOwnProperty('content') ? '' :
+    typeof args.content === 'object' ? group(args.content) : args.content || '';
 
-export const tag_open = (name: string = '', attrs: { [k: string]: any } = {}) => `<${name}${toKV(attrs)}>`;
-export const tag = (name: string = '', attrs: { [k: string]: any } = {}, content: any) => `${tag_open(name, attrs)}${get_content(content)}</${name}>`;
+// CORE
+export const tag_open = (name: string = '', args: Args = {}) =>
+    `<${name}${args.hasOwnProperty('attrs') && Object.keys(args.attrs!).length > 0 ? ' ' + toKV(args.attrs) : ''}>`;
+export const tag = (name: string = '', args?: Args) => `${tag_open(name, args)}${get_content(args)}</${name}>`;
 
-export const br = () => tag_open('br');
-export const img = (url: string = '') => tag_open('img', {src: url});
-export const ul = (attrs: { [k: string]: any } = {}, content: any = '') => tag('ul', attrs, content);
-export const li = (attrs: { [k: string]: any } = {}, content: any = '') => tag('li', attrs, content);
-export const div = (attrs: { [k: string]: any } = {}, content: any = '') => tag('div', attrs, content);
-export const a = (url: string = '', content: any = '') => tag('a', {href: url}, content);
+// COMMON TAGS
+export const br = () => tag_open('br', {attrs: {}, content: []});
+export const img = (src: string = '') => tag_open('img', {attrs: {src: src}, content: []});
+export const ul = (args: Args = {attrs: {}, content: []}) => tag('ul', args);
+export const li = (args = {attrs: {}, content: []}) => tag('li', args);
+export const div = (args: Args = {attrs: {}, content: []}) => tag('div', args);
+export const a = (href: string = '', content: any = '') => tag('a', {attrs: {href: href}, content: content});
+export const script = (src: string, content?: any) => tag('script', {attrs: {src: src}, content: content})
